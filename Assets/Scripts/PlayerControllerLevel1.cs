94f68b033f84b971ab3fdf6a64e4b1ef6ef3aa54
Jakub Stasiorowski <kuba.stasiorowski@wp.pl>
2022-10-11 15:39:57 +0200

    Simple jumping

--- Assets/Scripts/PlayerControllerLevel1.cs
+++ Assets/Scripts/PlayerControllerLevel1.cs
@@ -4,9 +4,40 @@ using UnityEngine;
 
 public class PlayerControllerLevel1 : MonoBehaviour
 {
-    public float moveSpeed = 0.1f;
-    public float jumpForce = 6f;
+    [SerializeField] float groundedTolerance = 0.7f;
+    [SerializeField] float groundedSideTolerance = 0.5f;
+    [SerializeField] float jumpForce = 6f;
+    [SerializeField] LayerMask groundLayer;
+    [SerializeField] float moveSpeed = 0.1f;
+
     private Rigidbody2D rigidBody;
+
+    public void Jump()
+    {
+        if (!IsGrounded())
+            return; // no double-jump
+
+        Debug.Log("Jumping");
+        rigidBody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+    }
+
+    private bool IsGrounded()
+    {
+        //Vector3 raycastTop = transform.position;
+        //Vector3 raycastBottom = raycastTop + Vector3.down * groundedTolerance;
+        //if(Physics.CapsuleCast(raycastTop, raycastBottom, groundedSideTolerance, Vector3.down, groundedTolerance, groundLayer.value))
+        //{
+        //    return true;
+        //}
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, groundedTolerance, groundLayer.value);
+
+        if (hit.collider != null)
+        {
+            return true;
+        }
+        Debug.Log("Air-borne");
+        return false;
+    }
 
     void Awake()
     {
@@ -15,19 +46,23 @@ public class PlayerControllerLevel1 : MonoBehaviour
 
     void Update()
     {
-        //if (Input.GetKey(KeyCode.RightArrow))
-        //{
-        //    transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
-        //}
-        //if (Input.GetKey(KeyCode.LeftArrow))
-        //{
-        //    transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
-        //}
-        // powy¿ej wersja dok³adnie wg instrukcji
         transform.Translate(Vector3.right * Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime);
         if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space))
         {
-            rigidBody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            Jump();
         }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        // Draw grounded tolerance
+        Gizmos.color = Color.blue;
+
+        Vector3 groundedExtent = transform.position + Vector3.down * groundedTolerance;
+
+        Gizmos.DrawLine(transform.position, groundedExtent);
+
+        Gizmos.DrawLine(groundedExtent, groundedExtent + Vector3.left * groundedSideTolerance);
+        Gizmos.DrawLine(groundedExtent, groundedExtent - Vector3.left * groundedSideTolerance);
     }
 }
