e839ef1277a6e4bf99908b35806be9626651fb9a
Adam Leczkowski <58361333+nne-nne@users.noreply.github.com>
2022-10-23 20:27:42 +0200

    flame shake

---
+++ Assets/Scripts/FlameShake.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlameShake : MonoBehaviour
+{
+    [SerializeField] private float speed = 1.0f;
+    [SerializeField] private float minIntensity = 0f, maxIntensity = 1f;
+    [SerializeField] private float exp = 1;
+
+    private SpriteRenderer rend;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float noiseVal = Mathf.PerlinNoise(Time.time * speed, Time.time * speed);
+        noiseVal = Mathf.Pow(noiseVal, exp);
+        noiseVal = Mathf.Lerp(minIntensity, maxIntensity, noiseVal);
+        rend.color = new Color(rend.color.r, rend.color.g, rend.color.b, noiseVal);
+    }
+}
