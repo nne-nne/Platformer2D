2c650c772978498a5c59f19704a30857b7b14c30
Jakub Stasiorowski <kuba.stasiorowski@wp.pl>
2022-11-01 12:01:30 +0100

    Level generation with one segment prefab

---
+++ Assets/Scripts/LevelGeneration/LeaveTrigger.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeaveTrigger : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        GameObject other = collision.gameObject;
+        Debug.Log(other);
+        if (other.CompareTag(GameTags.PlayerTag))
+        {
+            LevelGenerator.Instance.AddPiece();
+            LevelGenerator.Instance.RemoveOldestPiece();
+        }
+    }
+}
